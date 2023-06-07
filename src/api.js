d4200f7a28bd5cc688e0df27683d4612bfa0b59d
SARNABH HALDAR <105656445+CODERdotEXE@users.noreply.github.com>
2023-06-07 12:23:50 +0530

    Delete src directory

--- src/api.js
+++
@@ -1,6 +0,0 @@
-export async function fetchTVShows(query) {
-    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
-    const data = await response.json();
-    return data;
-  }
-  
